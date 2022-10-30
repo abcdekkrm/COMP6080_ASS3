0fcc4c2d5c2a87522c43c64fa0cd7129833392fb
eggboy3700 <pomeranians37@gmail.com>
2022-10-30 23:19:18 +1100

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
 import './App.css';
+import Nav from './Nav';
+import {
+  createMuiTheme,
+} from '@material-ui/core/styles';
+
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: {
+//       main: '#',
+//     },
+//     secondary: {
+//       main: '#',
+//     },
+//   },
+// });
 
 function App() {
-  return <></>;
+  return (
+    <Nav/>
+  )
 }
 
 export default App;
