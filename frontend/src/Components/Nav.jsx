102de10bb6388b3001a75e03770ee07374b7a5a1
Hsin Chen <sonia.chen1@student.unsw.edu.au>
2022-11-09 15:44:37 +1100

    delete create button

--- frontend/src/Components/Nav.jsx
+++ frontend/src/Components/Nav.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Login from '../Screens/Login';
 import ProfileMenu from './ProfileMenu';
 import EditListing from '../Screens/EditListing'
-import CreateListing from '../Screens/CreateListing'
 import {
   AppBar, Toolbar, Button, Typography
 } from '@material-ui/core';
@@ -11,7 +10,6 @@ import logo from '../Assets/logo.svg';
 function Nav () {
   const [loginOpen, setLoginOpen] = useState(false);
   const [editOpen, setEditOpen] = useState(false);
-  const [createOpen, setCreateOpen] = useState(false);
 
   const logged = localStorage.getItem('logged');
 
@@ -36,10 +34,8 @@ function Nav () {
         </Toolbar>
         </AppBar>
         {/* for testing edit listing */}
-        <Button onClick={() => setCreateOpen(true)}>Create</Button>
         <Button onClick={() => setEditOpen(true)}>Edit</Button>
         {editOpen ? <EditListing closeEditPopup={() => setEditOpen(false)} listingID={'564534127'} /> : null}
-        {createOpen ? <CreateListing closeCreatePopup={() => setCreateOpen(false)} /> : null}
      </nav>
     </header>
   )
